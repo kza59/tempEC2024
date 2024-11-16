f65648afc3e9271e0f8afe7ce05230b19af926f6
kane <kza59@sfu.ca>
2024-11-16 11:35:40 -0800

    basic implementation

--- peer.cpp
+++ peer.cpp
@@ -10,85 +10,153 @@
 
 using namespace std;
 
+string name;
+int PORT;
+
 void sending() {
+    char buffer[10000] = {0};
 
+    int PORT_server;
+    cout << "Enter the port to send a message: ";
+    cin >> PORT_server;
 
+    int sock = 0;
+    struct sockaddr_in serv_addr;
+    char message[1024] = {0};
 
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("Socket creation error");
+        return;
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(PORT_server);
+
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        perror("Connection Failed");
+        close(sock);
+        return;
+    }
+
+    cin.ignore();
+    cout << "Enter your message: ";
+    cin.getline(message, 1024);
+
+    snprintf(buffer, sizeof(buffer), "%s [PORT:%d] says: %s", name.c_str(), PORT, message);
+    send(sock, buffer, strlen(buffer), 0);
+    cout << "Message sent!" << endl;
+
+    close(sock);
 }
-void receiving() {
+
+void receiving(int SFD) {
+    struct sockaddr_in addr;
+    int addrlen = sizeof(addr);
+    char buffer[10000] = {0}; // Buffer for incoming messages
+    fd_set currentSockets, readySockets;
+
+    FD_ZERO(&currentSockets);
+    FD_SET(SFD, &currentSockets);
+
+    for (;;) {
+        readySockets = currentSockets;
+
+        if (select(FD_SETSIZE, &readySockets, NULL, NULL, NULL) < 0) {
+            perror("Select error");
+            exit(EXIT_FAILURE);
+        }
+
+        for (int i = 0; i < FD_SETSIZE; ++i) {
+            if (FD_ISSET(i, &readySockets)) {
+                if (i == SFD) {
+    
+                    int clientSocket = accept(SFD, (struct sockaddr *)&addr, (socklen_t *)&addrlen);
+                    if (clientSocket < 0) {
+                        perror("Accept error");
+                        continue;
+                    }
+                    FD_SET(clientSocket, &currentSockets);
+                } else {
+
+                    int value = recv(i, buffer, sizeof(buffer), 0);
+                    if (value <= 0) {
+                        close(i);
+                        FD_CLR(i, &currentSockets);
+                    } else {
+                        cout << "Received: " << buffer << endl;
+                        memset(buffer, 0, sizeof(buffer)); // Clear buffer
+                    }
+                }
+            }
+        }
+    }
+}
+
+void *receiveThread(void *serverFileDes) {
+    int SFD = *((int *)serverFileDes);
+    receiving(SFD);
+    return nullptr;
+}
+
+int main() {
+    int serverFileDes;
+    struct sockaddr_in addr;
+
+    cout << "Enter your name: ";
+    cin >> name;
+
+    cout << "Enter your port number: ";
+    cin >> PORT;
+
+    if ((serverFileDes = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
 
 
     
-}
-int main() {
-    string name = "";
-    int portNum = 0;
-    int serverFileDes, newSocket, valueRead;
-    struct sockaddr_in addr;
-
-
-    //getting name
-    cout << "enter name" << endl;
-    cin >> name;
-    cout << "name instantiated as: " << name << endl;
-
-    //getting port number
-    cout << "enter port number" << endl;
-    cin >> portNum;
-    cout << "port number instantiated as: " << portNum << endl;
-
-    //instantiating the server file descriptor
-    serverFileDes = socket(AF_INET, SOCK_STREAM, 0);
-    cout << "server file descriptor instantiated as: " << serverFileDes << endl;
-    // if(serverFileDes == -1) {
-    //     errno = EBADF;
-    //     exit(EXIT_FAILURE);
-    // }
     addr.sin_family = AF_INET;
     addr.sin_addr.s_addr = INADDR_ANY;
-    //converts to network byte order
-    addr.sin_port = htons(portNum);
+    addr.sin_port = htons(PORT);
 
-    if(bind(serverFileDes, (struct sockaddr *)&addr, sizeof(addr)) < 0)
-    {
-        perror("SERVER FILE DESCRIPTOR BINDING FAILED");
-        exit(EXIT_FAILURE);
-    }
-    if(listen(serverFileDes, 5) < 0) {
-        perror("LISTEN");
-        exit(EXIT_FAILURE);
-    }
-    cout << "fin" << endl;
-
-
-    while(true) {
-    string input = "";
-    cout << "enter an input. This input can either be 0(quit/q) or 1(send/s)" << endl;
-    cin >> input;
-    // tolower(input);
-    for(auto& item : input) {
-        tolower(item);
-    }
-    if(input == "quit" || input == "q" || input == "0") {
-        cout << "quitting" << endl;
+    if (bind(serverFileDes, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+        perror("Bind failed");
         close(serverFileDes);
-        break;
-    }
-    else if(input == "send" || input == "s" || input == "1") {
-        cout << "sending" << endl;
-        sending();
+        exit(EXIT_FAILURE);
     }
 
+    if (listen(serverFileDes, 5) < 0) {
+        perror("Listen failed");
+        close(serverFileDes);
+        exit(EXIT_FAILURE);
     }
-    cout << "ended the p2p communication" << endl;
+
+    pthread_t threadID;
+    pthread_create(&threadID, NULL, receiveThread, &serverFileDes);
 
 
+    while (true) {
+        string input;
+        cout << "Enter a command (send/s, quit/q): ";
+        cin >> input;
 
+        for (auto &ch : input) {
+            ch = tolower(ch);
+        }
 
+        if (input == "quit" || input == "q") {
+            cout << "Quitting..." << endl;
+            close(serverFileDes);
+            break;
+        } else if (input == "send" || input == "s") {
+            sending();
+        } else {
+            cout << "Invalid command!" << endl;
+        }
+    }
 
-    // if((serverFileDes = so))
-
-
-
-
+    pthread_join(threadID, NULL); 
+    cout << "Peer-to-peer communication ended." << endl;
+    return 0;
 }
