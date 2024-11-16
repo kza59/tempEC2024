bd29eeed5af31c5bae0749ec821424c6e1be405e
kane <kza59@sfu.ca>
2024-11-16 10:50:04 -0800

    c1

---
+++ peer.cpp
@@ -0,0 +1,33 @@
+#include <sys/socket.h>
+#include <iostream>
+using namespace std;
+
+void sending() {
+
+
+
+}
+void receiving() {
+
+
+    
+}
+int main() {
+    string name = "";
+    int portNum = 0;
+
+    //getting name
+    cout << "enter name" << endl;
+    cin >> name;
+    cout << "name instantiated as: " << name << endl;
+
+    //getting port number
+    cout << "enter port number" << endl;
+    cin >> portNum;
+    cout << "port number instantiated as: " << portNum << endl;
+
+
+
+
+
+}
