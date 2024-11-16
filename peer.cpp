bfd55d3debfc085e1d9a89186df1cae76d0511d6
kane <kza59@sfu.ca>
2024-11-16 11:11:37 -0800

    c2

--- peer.cpp
+++ peer.cpp
@@ -1,5 +1,13 @@
 #include <sys/socket.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
 #include <iostream>
+
 using namespace std;
 
 void sending() {
@@ -15,6 +23,9 @@ void receiving() {
 int main() {
     string name = "";
     int portNum = 0;
+    int serverFileDes, newSocket, valueRead;
+    struct sockaddr_in addr;
+
 
     //getting name
     cout << "enter name" << endl;
@@ -26,6 +37,56 @@ int main() {
     cin >> portNum;
     cout << "port number instantiated as: " << portNum << endl;
 
+    //instantiating the server file descriptor
+    serverFileDes = socket(AF_INET, SOCK_STREAM, 0);
+    cout << "server file descriptor instantiated as: " << serverFileDes << endl;
+    // if(serverFileDes == -1) {
+    //     errno = EBADF;
+    //     exit(EXIT_FAILURE);
+    // }
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = INADDR_ANY;
+    //converts to network byte order
+    addr.sin_port = htons(portNum);
+
+    if(bind(serverFileDes, (struct sockaddr *)&addr, sizeof(addr)) < 0)
+    {
+        perror("SERVER FILE DESCRIPTOR BINDING FAILED");
+        exit(EXIT_FAILURE);
+    }
+    if(listen(serverFileDes, 5) < 0) {
+        perror("LISTEN");
+        exit(EXIT_FAILURE);
+    }
+    cout << "fin" << endl;
+
+
+    while(true) {
+    string input = "";
+    cout << "enter an input. This input can either be 0(quit/q) or 1(send/s)" << endl;
+    cin >> input;
+    // tolower(input);
+    for(auto& item : input) {
+        tolower(item);
+    }
+    if(input == "quit" || input == "q" || input == "0") {
+        cout << "quitting" << endl;
+        close(serverFileDes);
+        break;
+    }
+    else if(input == "send" || input == "s" || input == "1") {
+        cout << "sending" << endl;
+        sending();
+    }
+
+    }
+    cout << "ended the p2p communication" << endl;
+
+
+
+
+
+    // if((serverFileDes = so))
 
 
 
